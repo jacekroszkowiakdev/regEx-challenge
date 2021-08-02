dc3695de2a3adcba4ec66ca8f617f515efd7b65c
MishDish <misha.vucicevic@sennder.com>
2021-08-02 17:33:40 +0200

    Add 2 unit-tests for the chop method

--- tests/unit-tests/fizzbuzz.test.ts
+++
@@ -1,8 +0,0 @@
-// / <reference types="jest" />
-
-import { fizzBuzz } from "./fizzbuzz";
-
-test("FizzBuzz test", () => {
-    expect(fizzBuzz(2)).toBe("1 2 ");
-    expect(fizzBuzz(3)).toBe("1 2 Fizz ");
-});

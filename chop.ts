1ba5e0d2e23d475b3257ddd0c609acd34f14a6f1
MishDish <misha.vucicevic@sennder.com>
2021-08-02 14:34:57 +0200

    Clearn code

--- chop.ts
+++ chop.ts
@@ -1,20 +1,26 @@
+
+export interface IDataSet {
+    bread: string | number;
+    cheese: string | number;
+}
+
 function isEmpty(obj: object): boolean {
     return Object.keys(obj).length === 0;
 }
 
-let chop = (
+const chop = (
     template: string,
-    data: { [key: string]: string | number }
+    dataset: IDataSet
 ): string | number => {
     // check if parameters are not empty
-    if (!template || isEmpty(data)) {
+    if (!template || isEmpty(dataset)) {
         return "template and dataset are mandatory parameters";
     }
 
     // check if parameters are of correct type:
     if (
         typeof template !== "string" ||
-        (typeof data !== "object" && typeof data !== null)
+        (typeof dataset !== "object" && typeof dataset !== null)
     ) {
         return "The input arguments are of wrong type. Arguments can be only of type string and object";
     }
@@ -22,14 +28,15 @@ let chop = (
     // parse the template:
     const regEx = /\{{\w+\}}/gi;
     const trimWhiteSpace = /\s\s+/g;
-    const dataKeys = Object.keys(data);
+    const dataKeys = Object.keys(dataset);
+    const cleanDataset = dataset as any
 
     let mapObj: any = {};
-    dataKeys.forEach((elem) => (mapObj[`{{${elem}}}`] = data[elem]));
+    dataKeys.forEach((elem, index) => mapObj[`{{${elem}}}`] =  cleanDataset[elem]);
 
     //chain another regex with white space replacement replace(/\s\s+)/
     let choppedTemplate = template.replace(regEx, (item) => mapObj[item]);
-
+    
     return choppedTemplate;
 };
 

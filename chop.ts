c524b12427fb36b12875a16cb2062e8ff9c0b80c
Jacek Roszkowiak <jacekroszkowiakdev@gmail.com>
2021-08-02 19:07:03 +0200

    more test cases

--- chop.ts
+++ chop.ts
@@ -1,5 +1,6 @@
-const WRONG_ARG_TYPE_MSG = 'The input arguments are of wrong type. Please provide the input in right format!'
-const MISSING_ARGS_MSG = 'template and dataset are mandatory parameters'
+const WRONG_ARG_TYPE_MSG =
+    "The input arguments are of wrong type. Please provide the input in right format!";
+const MISSING_ARGS_MSG = "template and dataset are mandatory parameters";
 
 export interface IDataSet {
     bread: string | number;
@@ -10,10 +11,7 @@ function isEmpty(obj: object): boolean {
     return Object.keys(obj).length === 0;
 }
 
-const chop = (
-    template: string,§
-    dataset: IDataSet
-): string | number => {
+const chop = (template: string, dataset: IDataSet): string | number => {
     // check if parameters are not empty
     if (!template || isEmpty(dataset)) {
         return MISSING_ARGS_MSG;
@@ -31,14 +29,16 @@ const chop = (
     const regEx = /\{{\w+\}}/gi;
     const trimWhiteSpace = /\s\s+/g;
     const dataKeys = Object.keys(dataset);
-    const cleanDataset = dataset as any
+    const cleanDataset = dataset as any;
 
     let mapObj: any = {};
-    dataKeys.forEach((elem, index) => mapObj[`{{${elem}}}`] =  cleanDataset[elem]);
+    dataKeys.forEach(
+        (elem, index) => (mapObj[`{{${elem}}}`] = cleanDataset[elem])
+    );
 
     //chain another regex with white space replacement replace(/\s\s+)/
     let choppedTemplate = template.replace(regEx, (item) => mapObj[item]);
-    
+
     return choppedTemplate;
 };
 
--- tests/unit-tests/chop.test.ts
+++ tests/unit-tests/chop.test.ts
@@ -1,21 +1,47 @@
 // / <reference types="jest" />
 
-import  chop, { IDataSet } from "../../chop";
+import chop, { IDataSet } from "../../chop";
 
 test("Chop should return string message with correct values from the dataset", () => {
     let dataset: IDataSet = {
         bread: "baguette",
         cheese: "brie cheese",
-    }
+    };
     const myTemplate = "I like to eat {{bread}} and {{cheese}}";
-    expect(chop(myTemplate,dataset)).toBe("I like to eat baguette and brie cheese");
+    expect(chop(myTemplate, dataset)).toBe(
+        "I like to eat baguette and brie cheese"
+    );
 });
 
 test("Chop should return incorrect result if the string interpolations are with space between the value", () => {
     let dataset: IDataSet = {
         bread: "baguette",
         cheese: "brie cheese",
-    }
+    };
     const myTemplate = "I like to eat {{ bread }} and {{ cheese }}";
-    expect(chop(myTemplate,dataset)).not.toBe("I like to eat baguette and brie cheese");
+    expect(chop(myTemplate, dataset)).not.toBe(
+        "I like to eat baguette and brie cheese"
+    );
+});
+
+test("Chop should return incorrect result if the dataset object value or values are empty", () => {
+    let dataset: IDataSet = {
+        bread: "",
+        cheese: "",
+    };
+    const myTemplate = "I like to eat {{bread}} and {{cheese}}";
+    expect(chop(myTemplate, dataset)).not.toBe(
+        "I like to eat baguette and brie cheese"
+    );
+});
+
+test("Chop should return incorrect result if user input does not match dataset keynames", () => {
+    let dataset: IDataSet = {
+        bread: "baguette",
+        cheese: "brie cheese",
+    };
+    const myTemplate = "I like to eat {{bread}} and {{butter}}";
+    expect(chop(myTemplate, dataset)).not.toBe(
+        "I like to eat baguette and butter"
+    );
 });
